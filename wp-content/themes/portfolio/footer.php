3f0677940cd63ffc8314dace4b934841a2faad8c
Samantha Baehr <samantha-baehr@Samanthas-MacBook-Pro.local>
2015-04-19 19:26:39 -0500

    Samantha Nissen Portfolio
    Initial Build

---
+++ wp-content/themes/portfolio/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package portfolio
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<nav class="footer-nav">
+			<a href="#">Home</a>
+			<a href="#">Work</a>
+			<a href="#">Contact</a>
+</nav>
+<div class="site-info">
+			<p>Samantha Nissen</p>
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/portfolio/work.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template Name: Work
+ */
+?>
+
+<?php
+ get_header();
+?>
+
+  <div class="content-area">
+    <?php if( get_field('work-image') ): ?>
+      <div class="work-img-container">
+<img src="http://localhost:8888/samantha-nissen/wp-content/uploads/2015/04/noel-nissen.jpg" />
+      </div>
+<?php endif; ?>
+    <div class="sidebar">
+      <h2>
+        About the Project
+      </h2>
+      <div class="about">
+      <?php the_field('about'); ?>
+      </div>
+      <h2>
+        Project Details
+      </h2>
+        <div class="role">
+      <h3>
+        Role
+      </h3>
+      <?php the_field('role'); ?>
+        </div>
+        <div class="skills">
+      <h3>
+        Skills
+      </h3>
+      <?php the_field('skills'); ?>
+        </div>
+        <div class="client">
+      <h3>
+        Client
+      </h3>
+      <?php the_field('client'); ?>
+        </div>
+      <button class="pure-button">A Pure Button</button>
+  </div>
+</div>
+
+
+<?php
+ get_footer();
+ ?>
